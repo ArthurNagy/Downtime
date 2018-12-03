e56d7d3b75027aeb50b4f35bb3c9ca21bbfd61df
Arthur Nagy <arthur.93.nagy@gmail.com>
2018-12-03 11:07:00 +0200

    Fix incorrect data queried for unlocks & notifications count

--- app/src/main/java/me/arthurnagy/downtime/core/StatsRepository.kt
+++ app/src/main/java/me/arthurnagy/downtime/core/StatsRepository.kt
@@ -2,21 +2,16 @@ package me.arthurnagy.downtime.core
 
 import android.app.usage.UsageEvents
 import android.app.usage.UsageStatsManager
-import android.app.usage.UsageStatsManager.INTERVAL_DAILY
 import android.os.Build
 import androidx.annotation.RequiresApi
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Date
-import java.util.Locale
+import java.util.*
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
 class StatsRepository(private val usageStatsManager: UsageStatsManager) {
 
-    private val startTime: Long
-    private val endTime: Long
-    private val formatter = SimpleDateFormat("yyyy.MM.dd HH:mm", Locale.getDefault())
+    private val startTimeToday: Long
+    private val endTimeToday: Long
 
     init {
         val calendar = Calendar.getInstance()
@@ -25,28 +20,33 @@ class StatsRepository(private val usageStatsManager: UsageStatsManager) {
         calendar.set(Calendar.MINUTE, 0)
         calendar.set(Calendar.SECOND, 0)
         calendar.set(Calendar.MILLISECOND, 0)
-        endTime = calendar.timeInMillis
+        endTimeToday = calendar.timeInMillis
         calendar.add(Calendar.DAY_OF_MONTH, -1)
-        startTime = calendar.timeInMillis
-        println("LOFASZ: begin: ${formatter.format(Date(startTime))}, end: ${formatter.format(Date(endTime))}")
+        startTimeToday = calendar.timeInMillis
     }
 
     @RequiresApi(Build.VERSION_CODES.P)
-    suspend fun getTodaysUnlockCount() = suspendCoroutine<Int> { continuation ->
-        val count = usageStatsManager.queryEventStats(INTERVAL_DAILY, startTime, endTime)
-            .filter { it.eventType == UsageEvents.Event.SCREEN_INTERACTIVE }
-            .sumBy { it.count }
-        continuation.resume(count)
-    }
-
-    @RequiresApi(Build.VERSION_CODES.P)
-    suspend fun getTodaysNotificationCount() = suspendCoroutine<Int> {
-        val usageEvents = usageStatsManager.queryEvents(startTime, endTime)
+    suspend fun getTodaysUnlockCount() = suspendCoroutine<Int> {
+        val usageEvents = usageStatsManager.queryEvents(startTimeToday, endTimeToday)
         var count = 0
         while (usageEvents.hasNextEvent()) {
             val event = UsageEvents.Event()
             usageEvents.getNextEvent(event)
-            if (event.eventType == 12) {// UsageEvents.Event.NOTIFICATION_INTERRUPTION
+            if (event.eventType == UsageEvents.Event.KEYGUARD_HIDDEN) {
+                count++
+            }
+        }
+        it.resume(count)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    suspend fun getTodaysNotificationCount() = suspendCoroutine<Int> {
+        val usageEvents = usageStatsManager.queryEvents(startTimeToday, endTimeToday)
+        var count = 0
+        while (usageEvents.hasNextEvent()) {
+            val event = UsageEvents.Event()
+            usageEvents.getNextEvent(event)
+            if (event.eventType == 12 && event.packageName != "android") {// UsageEvents.Event.NOTIFICATION_INTERRUPTION
                 count++
             }
         }
